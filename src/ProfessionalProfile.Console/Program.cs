e9c92d56b7a3df2520c3c7f5df33462f417ecebe
DESKTOP-KSDU36P\Augustin Nshimiyiman <addeodatus01@gmail.com>
2017-07-19 12:27:24 -0500

    Add project files.

---
+++ src/ProfessionalProfile.Console/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProfessionalProfile.Console
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+    }
+}
